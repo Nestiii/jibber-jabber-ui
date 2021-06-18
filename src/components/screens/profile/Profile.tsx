6c33f1fb76883bd9871cd10aa4041d87c4fca3ff
facundo <facundo@respond-software.com>
2021-06-18 13:23:58 -0300

    Edit profile integrated

--- src/components/screens/profile/Profile.tsx
+++ src/components/screens/profile/Profile.tsx
@@ -1,47 +1,90 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './Profile.scss';
 import {NeuInput} from '../../common/Input/Input';
 import {NeuButton} from '../../common/Button/Button';
 import {withRouter} from 'react-router-dom';
 import {ic_account_circle} from 'react-icons-kit/md/ic_account_circle'
 import Icon from 'react-icons-kit';
+import axios from 'axios';
+import {getConfig, getUsername, url} from '../../../utils';
+
+interface UserProps {
+    email: string,
+    firstName: string,
+    id: string,
+    lastName: string,
+    username: string
+}
+
+const initialUserState = {
+    email: '',
+    firstName: '',
+    id: '',
+    lastName: '',
+    username: ''
+}
 
 const Profile = () => {
 
     const [editing, setEditing] = useState(false);
-    const [userName, setUserName] = useState('User6543');
-    const [name, setName] = useState('John Doe');
-    const [email, setEmail] = useState('johndoe@gmail.com');
+    const [user, setUser] = useState<UserProps>({...initialUserState});
+    const [loading, setLoading] = useState(false);
 
     const handleSave = () => {
-        setEditing(false)
+        setLoading(true)
+        axios.post(url + 'users/edit', {...user}, getConfig())
+            .then((res: {data: UserProps}) => {
+                setUser(res.data)
+                setEditing(false)
+                setLoading(false)
+            })
+    }
+
+    useEffect(() => {
+        axios.get(url + 'users/by-username/' + getUsername(), getConfig())
+            .then((res: {data: UserProps}) => {
+                setUser(res.data)
+            })
+    }, [])
+
+    const handleUserChange = (field: keyof UserProps, value: string) => {
+        const temp = {...user}
+        temp[field] = value
+        setUser({...temp})
     }
 
     return (
         <div className={'profile'}>
             <div className={'profile-card'}>
                 <Icon icon={ic_account_circle} style={{color: '#f6cc35', marginBottom: 20}} size={100} />
-                <span className={'input-label'}>User name</span>
+                <span className={'input-label'}>First name</span>
                 <NeuInput
-                    onChange={(e) => setUserName(e.target.value)}
-                    value={userName}
+                    onChange={(e) => handleUserChange('firstName', e.target.value)}
+                    value={user.firstName}
                     disabled={!editing}
                 />
-                <span className={'input-label'}>Name</span>
+                <span className={'input-label'}>Last name</span>
                 <NeuInput
-                    onChange={(e) => setName(e.target.value)}
-                    value={name}
+                    onChange={(e) => handleUserChange('lastName', e.target.value)}
+                    value={user.lastName}
+                    disabled={!editing}
+                />
+                <span className={'input-label'}>Username</span>
+                <NeuInput
+                    onChange={(e) => handleUserChange('username', e.target.value)}
+                    value={user.username}
                     disabled={!editing}
                 />
                 <span className={'input-label'}>Email</span>
                 <NeuInput
-                    onChange={(e) => setEmail(e.target.value)}
-                    value={email}
+                    onChange={(e) => handleUserChange('email', e.target.value)}
+                    value={user.email}
                     disabled={!editing}
                 />
                 <NeuButton
                     label={editing ? 'Save' : 'Edit'}
                     onClick={() => editing ? handleSave() : setEditing(true)}
+                    loading={loading}
                 />
             </div>
         </div>
