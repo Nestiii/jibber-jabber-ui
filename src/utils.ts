4deebc3bf73091f803e625bbe155e90796517b12
facundo <facundo@respond-software.com>
2021-07-08 17:17:00 -0300

    fixed prod url

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-export const url = 'http://jibberjabber-dev.sytes.net/api/'
+export const url = 'http://jibberjabber.zapto.org/api/'
 
 export const getConfig = () => ({headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')}})
 
